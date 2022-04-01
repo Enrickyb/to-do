2c9c0d94f3252392ea17f94b6ccd8a640a7c1e23
Enrickyb <80066756+Enrickyb@users.noreply.github.com>
2022-04-01 10:49:25 -0400

    att commit

--- src/Todo.js
+++ src/Todo.js
@@ -53,14 +53,20 @@ export default function Todo() {
     function onHideModal(){ 
       setShowModal(false)
   }
+  var data = new Date();
+  var dia = String(data.getDate()).padStart(2, '0');
+  var mes = String(data.getMonth() + 1).padStart(2, '0');
+  var ano = data.getFullYear();
+  var dataAtual = dia + '/' + mes + '/' + ano;
 
   return (
 
     <div className="container">
       <header className="header">
         <h1>To do</h1>
-        <button className="addButton" onClick={()=>{setShowModal(true)}}>+</button>
+        <p>{dataAtual}</p>
       </header>
+      <div className="addcontainer"><p>Add task</p><button className="addButton" onClick={()=>{setShowModal(true)}}>+</button></div>
       <List onCheckItem={onCheckItem} onItemRemove={onItemRemove} items={items}></List>
       <Modal show={showModal} onHideModal={onHideModal}>
         <TodoForm onAddItem={onAddItem}></TodoForm> </Modal>
